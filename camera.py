41cf79117633150a8e475d6daff237d7e883062b
Paul Ambrose <pambrose@mac.com>
2017-01-25 10:41:40 -0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -9,23 +9,26 @@ class Camera(object):
     def __init__(self, src=0, use_picamera=True, resolution=(320, 240), framerate=32):
         if is_raspi():
             from imutils.video import VideoStream
-            # initialize the video stream and allow the cammera sensor to warmup
-            self.__vs = VideoStream(src=src, usePiCamera=use_picamera, resolution=resolution,
+            # Initialize the video stream
+            self.__vs = VideoStream(src=src,
+                                    usePiCamera=use_picamera,
+                                    resolution=resolution,
                                     framerate=framerate).start()
+            # Allow the cammera sensor to warmup
             time.sleep(2.0)
         else:
-            self.__vp = cv2.VideoCapture(0)
+            self.__vc = cv2.VideoCapture(0)
 
     def is_open(self):
-        return True if is_raspi() else self.__vp.isOpened()
+        return True if is_raspi() else self.__vc.isOpened()
 
     def read(self):
-        return self.__vs.read() if is_raspi() else self.__vp.read()[1]
+        return self.__vs.read() if is_raspi() else self.__vc.read()[1]
 
     def close(self):
         if is_raspi():
             self.__vs.stop()
         else:
-            self.__vp.release()
+            self.__vc.release()
 
         cv2.destroyAllWindows()
