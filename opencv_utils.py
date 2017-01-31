c90685b0a60729f22430205b81779578ee23615d
Paul Ambrose <pambrose@mac.com>
2017-01-30 20:00:43 -0800

    Add get_moment()

--- opencv_utils.py
+++ opencv_utils.py
@@ -9,6 +9,14 @@ BLUE = (255, 0, 0)
 YELLOW = (0, 255, 255)
 
 
+def get_moment(contour):
+    moment1 = cv2.moments(contour)
+    area = int(moment1["m00"])
+    x = int(moment1["m10"] / area)
+    y = int(moment1["m01"] / area)
+    return contour, area, x, y
+
+
 def save_image(frame):
     file_name = "ct-{0}.png".format(datetime.datetime.now().strftime("%H-%M-%S"))
     cv2.imwrite(file_name, frame)
