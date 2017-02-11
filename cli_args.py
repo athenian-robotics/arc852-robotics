487d873a1755732550d7d5094e20755b1324147a
Paul Ambrose <pambrose@mac.com>
2017-02-11 15:08:16 -0800

    Hack around cv2 issue

--- cli_args.py
+++ cli_args.py
@@ -1,10 +1,17 @@
 import argparse
 import logging
-
+import os
+import sys
 GRPC_PORT_DEFAULT = 50051
 
 # from grpc_support import GRPC_PORT_DEFAULT
-from image_server import HTTP_DELAY_SECS_DEFAULT, HTTP_HOST_DEFAULT, HTTP_TEMPLATE_DEFAULT, CAMERA_NAME_DEFAULT
+# Find where this package is installed
+__path = os.path.abspath(sys.modules[__name__].__file__)
+__dirname = os.path.dirname(__path)
+HTTP_TEMPLATE_DEFAULT = __dirname + "/html/image-reader.html"
+
+
+# from image_server import HTTP_DELAY_SECS_DEFAULT, HTTP_HOST_DEFAULT, HTTP_TEMPLATE_DEFAULT, CAMERA_NAME_DEFAULT
 
 
 def setup_cli_args(*args):
@@ -50,7 +57,7 @@ grpc_host = lambda p: p.add_argument("-g", "--grpc", dest="grpc_host", required=
 camera_name = lambda p: p.add_argument("-c", "--camera", dest="camera_name", required=True, help="Camera name")
 
 camera_name_optional = lambda p: p.add_argument("-c", "--camera", dest="camera_name", required=False,
-                                                default=CAMERA_NAME_DEFAULT, help="Camera name")
+                                                default="Unnamed", help="Camera name")
 
 mqtt_host = lambda p: p.add_argument("-m", "--mqtt", dest="mqtt_host", required=True, help="MQTT server hostname")
 
@@ -59,12 +66,12 @@ calib = lambda p: p.add_argument("-c", "--calib", default=False, action="store_t
 alternate = lambda p: p.add_argument("-a", "--alternate", default=False, action="store_true",
                                      help="Alternate servo actions [false]")
 
-http_host = lambda p: p.add_argument("-t", "--http", dest="http_host", default=HTTP_HOST_DEFAULT, required=False,
-                                     help="HTTP hostname:port [{0}]".format(HTTP_HOST_DEFAULT))
+http_host = lambda p: p.add_argument("-t", "--http", dest="http_host", default="localhost:8080", required=False,
+                                     help="HTTP hostname:port [{0}]".format("localhost:8080"))
 
-http_delay_secs = lambda p: p.add_argument("-s", "--delay", default=HTTP_DELAY_SECS_DEFAULT, type=float,
+http_delay_secs = lambda p: p.add_argument("-s", "--delay", default=0.25, type=float,
                                            dest="http_delay_secs",
-                                           help="HTTP delay secs [{0}]".format(HTTP_DELAY_SECS_DEFAULT))
+                                           help="HTTP delay secs [{0}]".format(0.25))
 
 http_file = lambda p: p.add_argument("-i", "--file", default=HTTP_TEMPLATE_DEFAULT, type=str,
                                      dest="http_file", help="HTTP template file")
