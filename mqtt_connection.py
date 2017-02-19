5b85072752714e78caadbe4888ec815a458b74bb
Paul Ambrose <pambrose@mac.com>
2017-02-18 17:52:57 -0800

    Cleanup disconnect message

--- mqtt_connection.py
+++ mqtt_connection.py
@@ -31,10 +31,9 @@ def on_publish(client, userdata, mid):
 
 def on_disconnect(client, userdata, rc):
     if userdata:
-        logging.info("{0} disconecting from {1}:{2}".format("Success" if rc == 0 else "Failure",
-                                                            userdata[PAHO_HOSTNAME], userdata[PAHO_PORT]))
+        logging.info("Disconecting from {0}:{1}".format(userdata[PAHO_HOSTNAME], userdata[PAHO_PORT]))
     else:
-        logging.info("{0} disconecting from MQTT broker".format("Success" if rc == 0 else "Failure"))
+        logging.info("Disconecting from MQTT broker")
 
 
 class MqttConnection(object):
