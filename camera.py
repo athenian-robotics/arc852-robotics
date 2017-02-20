fad67e4bbf37673b1738cd95a3b7dfc6397a37af
Paul Ambrose <pambrose@mac.com>
2017-02-20 11:34:39 -0800

    Test pi camera option

--- camera.py
+++ camera.py
@@ -1,12 +1,13 @@
 import time
 
-import cv2
-from utils import is_raspi
+from cv2 import VideoCapture, destroyAllWindows
 
 
 class Camera(object):
     def __init__(self, src=0, use_picamera=True, resolution=(320, 240), framerate=32):
-        if is_raspi() and False:
+        self._use_picamera = use_picamera
+
+        if self._use_picamera:
             from imutils.video import VideoStream
             # Initialize the video stream
             self.__vs = VideoStream(src=src,
@@ -16,18 +17,18 @@ class Camera(object):
             # Allow the cammera sensor to warmup
             time.sleep(2.0)
         else:
-            self.__vc = cv2.VideoCapture(0)
+            self.__vc = VideoCapture(0)
 
     def is_open(self):
-        return True if is_raspi() else self.__vc.isOpened()
+        return True if self._use_picamera else self.__vc.isOpened()
 
     def read(self):
-        return self.__vs.read() if is_raspi() else self.__vc.read()[1]
+        return self.__vs.read() if self._use_picamera else self.__vc.read()[1]
 
     def close(self):
-        if is_raspi():
+        if self._use_picamera:
             self.__vs.stop()
         else:
             self.__vc.release()
 
-        cv2.destroyAllWindows()
+        destroyAllWindows()
