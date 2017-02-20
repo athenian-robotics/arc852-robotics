aae2cf5c3fbac37e53fd8d6cf2c3eb3af1f703fc
Paul Ambrose <pambrose@mac.com>
2017-02-20 12:17:19 -0800

    Add exposure setting

--- camera.py
+++ camera.py
@@ -1,13 +1,14 @@
 import time
-
 from cv2 import VideoCapture, destroyAllWindows
+
+from utils import is_raspi
 
 
 class Camera(object):
     def __init__(self, src=0, use_picamera=True, resolution=(320, 240), framerate=32):
         self._use_picamera = use_picamera
 
-        if self._use_picamera:
+        if self.use_vs():
             from imutils.video import VideoStream
             # Initialize the video stream
             self.__vs = VideoStream(src=src,
@@ -18,16 +19,18 @@ class Camera(object):
             time.sleep(2.0)
         else:
             self.__vc = VideoCapture(0)
-            # self.__vc.set(15, -11)
+
+    def use_vs(self):
+        return self._use_picamera and is_raspi()
 
     def is_open(self):
-        return True if self._use_picamera else self.__vc.isOpened()
+        return True if self.use_vs() else self.__vc.isOpened()
 
     def read(self):
-        return self.__vs.read() if self._use_picamera else self.__vc.read()[1]
+        return self.__vs.read() if self.use_vs() else self.__vc.read()[1]
 
     def close(self):
-        if self._use_picamera:
+        if self.use_vs():
             self.__vs.stop()
         else:
             self.__vc.release()
