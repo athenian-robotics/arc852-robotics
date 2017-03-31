7d636081e4fa2c4f3f86028ec368112e85599e72
Paul Ambrose <pambrose@mac.com>
2017-03-31 12:16:19 -0700

    Add get_max_vertical_contours()

--- contour_finder.py
+++ contour_finder.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from constants import MINIMUM_PIXELS_DEFAULT, HSV_RANGE_DEFAULT
+from opencv_utils import contour_slope_degrees
 
 
 class ContourFinder(object):
@@ -43,5 +44,44 @@ class ContourFinder(object):
 
         # Return max contours
         eligible = [c for c in contours if cv2.moments(c)["m00"] >= self.__minimum_pixels]
+        val = sorted(eligible, key=lambda v: cv2.moments(v)["m00"], reverse=True)[:count]
+        return val if val else None
+
+    def get_max_vertical_contours(self, image, lower=None, upper=None, count=1):
+        # Convert from BGR to HSV colorspace
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        # Threshold the HSV image to get only target colors
+        in_range_mask = cv2.inRange(hsv_image,
+                                    lower if lower else self.__lower,
+                                    upper if upper else self.__upper)
+
+        # Bitwise-AND mask and original image
+        in_range_result = cv2.bitwise_and(image, image, mask=in_range_mask)
+
+        # Convert to grayscale
+        grayscale = cv2.cvtColor(in_range_result, cv2.COLOR_BGR2GRAY)
+
+        # Get all contours
+        contours = cv2.findContours(grayscale, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+
+        # cv2.imshow("HSV", hsv_image)
+        # cv2.imshow("Mask", in_range_mask)
+        # cv2.imshow("Res", in_range_result)
+        # cv2.imshow("Grayscale", grayscale)
+
+        verticals = []
+        for c in contours:
+            if c is None:
+                continue
+            slope, degrees = contour_slope_degrees(c)
+            if abs(degrees) < 70 or (slope is not None and abs(slope) < 20):
+                # logger.info("Slope: {0}".format(slope))
+                # logger.info("Degrees: {0}".format(degrees))
+                continue
+            verticals.append(c)
+
+        # Return max verticval contours
+        eligible = [c for c in verticals if cv2.moments(c)["m00"] >= self.__minimum_pixels]
         val = sorted(eligible, key=lambda v: cv2.moments(v)["m00"], reverse=True)[:count]
         return val if val else None
