a67e6b979fe3df0e23e0d089e427c5561b0077b0
ashah03 <aditshah00@gmail.com>
2018-02-03 15:39:50 -0800

    Remove opencv install on arm devices

--- setup.py
+++ setup.py
@@ -7,19 +7,40 @@ https://github.com/pypa/sampleproject
 
 from codecs import open
 from os import path
+import os
 
 # Always prefer setuptools over distutils
 from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
-
+if os.uname()[4].startswith('arm'):
+    reqs = [
+            'opencv-python>=3.4.0.12',
+            'numpy>=1.14.0',
+            'prometheus_client>=0.1.1',
+            'flask>=0.12.2',
+            'pyserial>=3.4',
+            'paho-mqtt>=1.3.1',
+            'requests>=2.18.4',
+            'imutils>=0.4.5',
+        ]
+else:
+    reqs = [
+            'numpy>=1.14.0',
+            'prometheus_client>=0.1.1',
+            'flask>=0.12.2',
+            'pyserial>=3.4',
+            'paho-mqtt>=1.3.1',
+            'requests>=2.18.4',
+            'imutils>=0.4.5',
+        ]
 with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='arc852-robotics',
-    version='1.0.12',
-    description='ARC852  Robotic Code',
+    version='1.0.13',
+    description='ARC852 Robotic Code',
     url='https://github.com/athenian-robotics/arc852-robotics',
     author='The Athenian School',
     author_email='pambrose@mac.com',
@@ -48,14 +69,5 @@ setup(
 
     packages=['arc852', ],
 
-    install_requires=[
-        'opencv-python>=3.4.0.12',
-        'numpy>=1.14.0',
-        'prometheus_client>=0.1.1',
-        'flask>=0.12.2',
-        'pyserial>=3.4',
-        'paho-mqtt>=1.3.1',
-        'requests>=2.18.4',
-        'imutils>=0.4.5',
-    ],
+    install_requires=reqs,
 )
