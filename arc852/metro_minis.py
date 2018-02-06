cb0a6c800fe85122fb7d4602966aec0d6797c347
Paul Ambrose <pambrose@mac.com>
2018-02-05 21:49:09 -0800

    Update main()

--- arc852/metro_minis.py
+++ arc852/metro_minis.py
@@ -2,10 +2,15 @@
 
 from arc852.serial_reader import SerialReader, DEVICE, HWID, SN, MANF
 
-if __name__ == "__main__":
+
+def main():
     for p in SerialReader.metro_minis():
         print("Manufacturer: " + p[MANF])
         print("HWID: " + p[HWID])
         print("Device: " + p[DEVICE])
         print("SN: " + p[SN])
         print("\n")
+
+
+if __name__ == "__main__":
+    main()
