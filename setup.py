b9e53dccb884bfc24219508fdf7b5df74ad08c76
chafla <namen937@gmail.com>
2018-02-09 17:55:46 -0800

    Bump version number

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
 
 setup(
     name='arc852-robotics',
-    version='1.0.17',
+    version='1.0.18',
     description='ARC852 Robotic Code',
     url='https://github.com/athenian-robotics/arc852-robotics',
     author='The Athenian School',
